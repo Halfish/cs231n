2b18882ff34944c8e7a0bc2b53bd042b985fd31e
Halfish <zhangxb.sysu@gmail.com>
2017-02-13 11:24:01 +0800

    finished Softmax classifier

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,5 +1,5 @@
 import numpy as np
-from random import shuffle
+#from random import shuffle
 
 def softmax_loss_naive(W, X, y, reg):
   """
@@ -30,6 +30,17 @@ def softmax_loss_naive(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  N, C = X.shape[0], W.shape[1]
+  for i in range(N):
+      f = np.dot(X[i], W)
+      f -= np.max(f) # f.shape = C
+      loss = loss + np.log(np.sum(np.exp(f))) - f[y[i]]
+      dW[:, y[i]] -= X[i]
+      s = np.exp(f).sum()
+      for j in range(C):
+          dW[:, j] += np.exp(f[j]) / s * X[i]
+  loss = loss / N + 0.5 * reg * np.sum(W * W)
+  dW = dW / N + reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -54,6 +65,18 @@ def softmax_loss_vectorized(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   pass
+  N = X.shape[0]
+  f = np.dot(X, W) # f.shape = N, C
+  f -= f.max(axis = 1).reshape(N, 1)
+  s = np.exp(f).sum(axis = 1)
+  loss = np.log(s).sum() - f[range(N), y].sum()
+
+  counts = np.exp(f) / s.reshape(N, 1)
+  counts[range(N), y] -= 1
+  dW = np.dot(X.T, counts)
+
+  loss = loss / N + 0.5 * reg * np.sum(W * W)
+  dW = dW / N + reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
